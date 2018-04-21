a1aa55ecc145ce71700ce4cbef81cc733fa2c221
Chris Henry <cdhenry@gmail.com>
2018-04-21 14:45:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Write your solution here!
-const drivers =  ["Milo", "Otis", "Garfield"];
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+  name.push("Ralph")
+}
+
+function destructivelyPrependDriver(name){
+  name.shift
+}
