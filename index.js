8a63521cabda4ec489d5296cdd5b581b2a243960
Chris Henry <cdhenry@gmail.com>
2018-04-21 14:48:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function destructivelyPrependDriver(name){
 }
 
 function destructivelyRemoveLastDriver(){
-  drivers.pop;
+  drivers.pop();
 }
 
 function destructivelyRemoveFirstDriver(){
-  drivers.shift;
+  drivers.shift();
 }
